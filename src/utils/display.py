91a7005287620225eb78f24f23a4dbf4ad99a462
Sarim Abbas <sarim.stays@gmail.com>
2019-09-14 12:47:21 -0400

    fix colors

--- src/utils/display.py
+++ src/utils/display.py
@@ -5,7 +5,7 @@ class Display:
         self.yTop = yTop
         self.dWidth = dWidth
         self.dHeight = dHeight
-        self.rgb = (random(255) * 1.3,) * 3
+        self.rgb = (random(255) * 1.3, random(255) * 1.3, random(255) * 1.3)
         self.exists = True
 
     def __repr__(self):
@@ -18,7 +18,7 @@ class Display:
 
     def draw(self):
         # rectangle
-        fill(color(self.rgb[0], self.rgb[2], self.rgb[1]))
+        fill(color(self.rgb[0], self.rgb[1], self.rgb[2]))
         rect(self.xLeft, self.yTop, self.dWidth, self.dHeight)
 
         # add text
